c0b841072e020c0096c6b7fde8094e5647725466
Imran Dawud <imda3289@colorado.edu>
2019-04-02 22:00:50 -0600

    DataBaseSystem Files

---
+++ almond.sh
@@ -0,0 +1,7 @@
+$ SCALA_VERSION=2.12.8 ALMOND_VERSION=0.2.1
+$ coursier bootstrap \
+    -r jitpack \
+    -i user -I user:sh.almond:scala-kernel-api_$SCALA_VERSION:$ALMOND_VERSION \
+    sh.almond:scala-kernel_$SCALA_VERSION:$ALMOND_VERSION \
+    --sources --default=true \
+    -o almond
